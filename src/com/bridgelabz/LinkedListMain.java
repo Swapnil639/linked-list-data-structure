2aa0559ca280bd500e5a8998ef0b437b92c36aa5
Swapnil639 <swapnilpatil631997@gmail.com>
2022-06-22 22:44:52 +0530

    Added :Create linked list

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -1,7 +1,14 @@
 package com.bridgelabz;
 
+import java.util.LinkedList;
+
 public class LinkedListMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Linked List program");
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(56);
+        list.add(30);
+        list.add(70);
+        System.out.println("Simple Linked list is : " + list);
     }
 }
